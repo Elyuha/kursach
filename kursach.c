75f0a1ad9c1ca33beefe9b1b3e0b27bbd991cac4
Elyuha <32392043+Elyuha@users.noreply.github.com>
2018-11-21 20:03:09 +0300

    Add files via upload

--- kursach.c
+++ kursach.c
@@ -11,9 +11,10 @@ struct base
 	char author[20];
 	char name[30];
 	char gerne[15];
-}list;
+}*RecordLibrary;
 
-long int count;
+long count;
+
 
 int replenishment();
 int editing();
@@ -29,28 +30,33 @@ int main()
 	FILE *BASE;
 	int c = 0;
 	long int len;
+	system("CLS");
+	BASE = fopen("base.bin", "rb");
+	if (BASE == NULL)
+		return 1;
+	fseek(BASE, 0, SEEK_END);
+	len = ftell(BASE);
+	count = len / 65;
+	RecordLibrary = (struct base*)malloc(sizeof(struct base) * count);
+
+	fseek(BASE, 0, SEEK_SET);
+
+	for (int i = 0; i < count; i++)
+	{
+		fread(RecordLibrary + i, sizeof(struct base), 1, BASE);
+	}
 	while (c != 8)
 	{
 		system("CLS");
-		BASE = fopen("base.bin", "rb");
-		if (BASE == NULL)
-			return 1;
-		fseek(BASE, 0, SEEK_END);
-		len = ftell(BASE);
-		count = len / 65;
-		struct base *RecordLibrary = (struct base*)malloc(sizeof(struct base) * count);
-
-		fseek(BASE, 0, SEEK_SET);
 
 		for (int i = 0; i < count; i++)
 		{
-			fread(RecordLibrary + i, sizeof(struct base), 1, BASE);
 			printf("%3d|", i + 1);
 			printf("%-20s|", (RecordLibrary+i)->author);
 			printf("%-30s|", (RecordLibrary+i)->name);
 			printf("%s \n", (RecordLibrary+i)->gerne);
 		}
-		printf("Выберите дальнейшее действие: \n");
+		printf("\nВыберите дальнейшее действие: \n");
 		printf("1) Пополнить базу новой композицией \n");
 		printf("2) Отредактировать базу \n");
 		printf("3) Удалить композицию из базы \n");
@@ -99,75 +105,55 @@ int main()
 		}
 		}
 		system("CLS");
-		fclose(BASE);
 	}
-}
-
-
-int replenishment()
-{
-	FILE *BASE;
-	BASE = fopen("base.bin", "ab+");
+	fclose(BASE);
+	BASE = fopen("base.bin", "wb");
 	if (BASE == NULL)
 		return 1;
+	for (int i = 0; i < count; i++)
+	{
+		fwrite((RecordLibrary + i)->author, 1, sizeof((RecordLibrary + i)->author), BASE);
+		fwrite((RecordLibrary + i)->name, 1, sizeof((RecordLibrary + i)->name), BASE);
+		fwrite((RecordLibrary + i)->gerne, 1, sizeof((RecordLibrary + i)->gerne), BASE);
+	}
+
+	fclose(BASE);
+}
+//*****************************************
+int replenishment()
+{
 	system("CLS");
+	count++;
+	RecordLibrary = (struct base*)realloc(RecordLibrary, sizeof(struct base) * (count));
 	printf("Введите автора композиции:   ");
 	getchar();
-	gets(list.author);
+	gets((RecordLibrary + (count - 1))->author);
 	printf("Введите название композиции:   ");
-	//getchar();
-	gets(list.name);
+	gets((RecordLibrary + (count - 1))->name);
 	printf("Введите жанр композиции:   ");
-	//getchar();
-	gets(list.gerne);
-	fwrite(list.author, 1, sizeof(list.author), BASE);
-	fwrite(list.name, 1, sizeof(list.name), BASE);
-	fwrite(list.gerne, 1, sizeof(list.gerne), BASE);
-	fclose(BASE);
+	gets((RecordLibrary + (count - 1))->gerne);
 	return 0;
 }
-
+//*****************************************
 int editing()
 {
-	FILE *BASE;
 	int j = 0;
 	int k, num;
 
 	system("CLS");
-
-	BASE = fopen("base.bin", "rb");
-	if (BASE == NULL)
-		return 1;
-
-	struct base *RecordLibrary = (struct base*)malloc(sizeof(struct base) * count);
-
 	for (int i = 0; i < count; i++)
 	{
-		fread(RecordLibrary + i, sizeof(struct base), 1, BASE);
 		printf("%3d|", i + 1);
 		printf("%-20s|", (RecordLibrary + i)->author);
 		printf("%-30s|", (RecordLibrary + i)->name);
 		printf("%s \n", (RecordLibrary + i)->gerne);
 	}
 
-	fclose(BASE);
-
-	BASE = fopen("base.bin", "wb");
-	if (BASE == NULL)
-		return 1;
-
 	printf("\n0 - Отмена \n");
 	printf("Введите номер строки, которую хотите изменить - ");
 	scanf("%d", &k);
 	if (k == 0)
 	{
-		for (int i = 0; i < count; i++)
-		{
-			fwrite((RecordLibrary + i)->author, 1, sizeof((RecordLibrary + i)->author), BASE);
-			fwrite((RecordLibrary + i)->name, 1, sizeof((RecordLibrary + i)->name), BASE);
-			fwrite((RecordLibrary + i)->gerne, 1, sizeof((RecordLibrary + i)->gerne), BASE);
-		}
-		free(RecordLibrary);
 		return 0;
 	}
 
@@ -218,49 +204,28 @@ int editing()
 	if (num == 1)
 	{
 		system("CLS");
-		free(RecordLibrary);
 		editing();
 	}
-	for (int i = 0; i < count; i++)
-	{
-		fwrite((RecordLibrary + i)->author, 1, sizeof((RecordLibrary + i)->author), BASE);
-		fwrite((RecordLibrary + i)->name, 1, sizeof((RecordLibrary + i)->name), BASE);
-		fwrite((RecordLibrary + i)->gerne, 1, sizeof((RecordLibrary + i)->gerne), BASE);
-	}
-	free(RecordLibrary);
-	fclose(BASE);
 
 	return 0;
 }
-
+//*****************************************
 int deleting()
 {
-	FILE *BASE;
 	int j = 0;
 	int k;
 
 	system("CLS");
 
-	BASE = fopen("base.bin", "rb");
-	if (BASE == NULL)
-		return 1;
-
-	struct base *RecordLibrary = (struct base*)malloc(sizeof(struct base) * count);
 
 	for (int i = 0; i < count; i++)
 	{
-		fread(RecordLibrary + i, sizeof(struct base), 1, BASE);
 		printf("%3d|", i + 1);
 		printf("%-20s|", (RecordLibrary + i)->author);
 		printf("%-30s|", (RecordLibrary + i)->name);
 		printf("%s \n", (RecordLibrary + i)->gerne);
 	}
 
-	fclose(BASE);
-
-	BASE = fopen("base.bin", "wb");
-	if (BASE == NULL)
-		return 1;
 
 	printf("Введите номер строки, которую хотите удалить - ");
 	scanf("%d", &k);
@@ -269,41 +234,18 @@ int deleting()
 		*(RecordLibrary + (k-1)) = *(RecordLibrary + k);
 		k++;
 	}
-	for (int i = 0; i < count-1; i++)
-	{
-		fwrite((RecordLibrary + i)->author, 1, sizeof((RecordLibrary + i)->author), BASE);
-		fwrite((RecordLibrary + i)->name, 1, sizeof((RecordLibrary + i)->name), BASE);
-		fwrite((RecordLibrary + i)->gerne, 1, sizeof((RecordLibrary + i)->gerne), BASE);
-	}
-	free(RecordLibrary);
 
-	fclose(BASE);
+	count--;
+	RecordLibrary = (struct base*)realloc(RecordLibrary, sizeof(struct base) * count);
+
 	return 0;
 }
-
+//*****************************************
 int sortAuthor()
 {
-	FILE* BASE;
-
-	BASE = fopen("base.bin", "r");
-	if (BASE == NULL)
-		return 1;
 
 	char line1[20], line2[20];
 	struct base *str = (struct base*)malloc(sizeof(struct base) + 1); // для свапа структур 
-	struct base *RecordLibrary = (struct base*)malloc(sizeof(struct base) * count);
-
-	for (int i = 0; i < count; i++)
-	{
-		fread(RecordLibrary + i, sizeof(struct base), 1, BASE);
-	}
-
-	fclose(BASE);
-
-	BASE = fopen("base.bin", "w");
-	if (BASE == NULL)
-		return 1;
-
 
 	for (int i = 0; i < count; i++) //для нормальной сортировки без учёта регистра 
 	{
@@ -339,43 +281,17 @@ int sortAuthor()
 			}
 		}
 	}
-
-	for (int i = 0; i < count; i++)
-	{
-		fwrite((RecordLibrary + i)->author, 1, sizeof((RecordLibrary + i)->author), BASE);
-		fwrite((RecordLibrary + i)->name, 1, sizeof((RecordLibrary + i)->name), BASE);
-		fwrite((RecordLibrary + i)->gerne, 1, sizeof((RecordLibrary + i)->gerne), BASE);
-	}
-	free(RecordLibrary);
 	free(str);
-	fclose(BASE);
 	return 0;
 }
-
+//*****************************************
 int sortName()
 {
-	FILE* BASE;
-
-	BASE = fopen("base.bin", "r");
-	if (BASE == NULL)
-		return 1;
 
 	char line1[30], line2[30];
 	struct base *str = (struct base*)malloc(sizeof(struct base) + 1); 
-	struct base *RecordLibrary = (struct base*)malloc(sizeof(struct base) * count);
 
-	for (int i = 0; i < count; i++)
-	{
-		fread(RecordLibrary + i, sizeof(struct base), 1, BASE);
-	}
-
-	fclose(BASE);
-
-	BASE = fopen("base.bin", "w");
-	if (BASE == NULL)
-		return 1;
-
-
+	
 	for (int i = 0; i < count; i++) 
 	{
 		for (int k = 0; k < count - 1; k++)
@@ -410,42 +326,14 @@ int sortName()
 			}
 		}
 	}
-
-	for (int i = 0; i < count; i++)
-	{
-		fwrite((RecordLibrary + i)->author, 1, sizeof((RecordLibrary + i)->author), BASE);
-		fwrite((RecordLibrary + i)->name, 1, sizeof((RecordLibrary + i)->name), BASE);
-		fwrite((RecordLibrary + i)->gerne, 1, sizeof((RecordLibrary + i)->gerne), BASE);
-	}
-	free(RecordLibrary);
 	free(str);
-	fclose(BASE);
 	return 0;
 }
-
+//*****************************************
 int sortGerne()
 {
-	FILE* BASE;
-
-	BASE = fopen("base.bin", "r");
-	if (BASE == NULL)
-		return 1;
-
 	char line1[15], line2[15];
 	struct base *str = (struct base*)malloc(sizeof(struct base) + 1);
-	struct base *RecordLibrary = (struct base*)malloc(sizeof(struct base) * count);
-
-	for (int i = 0; i < count; i++)
-	{
-		fread(RecordLibrary + i, sizeof(struct base), 1, BASE);
-	}
-
-	fclose(BASE);
-
-	BASE = fopen("base.bin", "w");
-	if (BASE == NULL)
-		return 1;
-
 
 	for (int i = 0; i < count; i++)
 	{
@@ -481,42 +369,73 @@ int sortGerne()
 			}
 		}
 	}
-
-	for (int i = 0; i < count; i++)
-	{
-		fwrite((RecordLibrary + i)->author, 1, sizeof((RecordLibrary + i)->author), BASE);
-		fwrite((RecordLibrary + i)->name, 1, sizeof((RecordLibrary + i)->name), BASE);
-		fwrite((RecordLibrary + i)->gerne, 1, sizeof((RecordLibrary + i)->gerne), BASE);
-	}
-	free(RecordLibrary);
 	free(str);
-	fclose(BASE);
 	return 0;
 }
-
+//*****************************************
 int search()
 {
-	FILE *BASE;
-	BASE = fopen("base.bin", "rb");
-	if (BASE == NULL)
-		return 1;
 	int i, k = 0, j = 0, cnt = 0;
 	int flag = 0;
-	char input[3][31];
+	char *input1 = (char*)malloc(sizeof(char) * 1);
+	char *input2 = (char*)malloc(sizeof(char) * 1);
+	char *input3 = (char*)malloc(sizeof(char) * 1);
 	struct base *mass = (struct base*)malloc(sizeof(struct base) * count);
-	char array[68];
-	struct base *RecordLibrary = (struct base*)malloc(sizeof(struct base) * count);
+	char array[63];
+	////////////////////////////////////////////////////////////////////////////////
 
-	for (i = 0; i < count; i++)
-	{
-		fread(RecordLibrary + i, sizeof(struct base), 1, BASE);
-	}
 	printf("Введите ключевые слова: ");
 	getchar();
 	gets(array);
+	while (array[cnt] != '\0')
+	{
+		if (array[cnt] >= 'A' && array[cnt] <= 'Z')
+		{
+			array[cnt] += 'a' - 'A';
+		}
+		cnt++;
+	}
+	cnt = 0;
+	while (array[cnt] >= 'a' && array[cnt] <= 'z')
+	{
+		input1 = (char*)realloc(input1, sizeof(char) * (j + 1));
+		*(input1 + j) = array[cnt];
+		cnt++;
+		j++;
+	}
+	input1 = (char*)realloc(input1, sizeof(char) * (j + 1));
+	*(input1 + j) = '\0';
+	j = 0;
+	if (array[cnt] == '\n')
+		goto next;
+	cnt++;
+
+	while (array[cnt] >= 'a' && array[cnt] <= 'z')
+	{
+		input2 = (char*)realloc(input2, sizeof(char) * (j + 1));
+		*(input2 + j) = array[cnt];
+		cnt++;
+		j++;
+	}
+	j = 0;
+
+	if (array[cnt] == '\n')
+		goto next;
+	cnt++;
+
+	while (array[cnt] >= 'a' && array[cnt] <= 'z')
+	{
+		input3 = (char*)realloc(input3, sizeof(char) * (j + 1));
+		*(input3 + j) = array[cnt];
+		cnt++;
+		j++;
+	}
+next:;
+	/////////////////////////////////////////////////////////////////////////////////////
 	printf("\n");
 	for (i = 0; i < count; i++)
 	{
+		j = 0;
 		while ((RecordLibrary + i)->author[j] != '\0')
 		{
 			if ((RecordLibrary + i)->author[j] >= 'A' && (RecordLibrary + i)->author[j] <= 'Z')
@@ -526,6 +445,7 @@ int search()
 			k++;
 			j++;
 		}
+		(mass + i)->author[j] = '\0';
 		j = 0;
 
 		while ((RecordLibrary + i)->name[j] != '\0')
@@ -537,6 +457,7 @@ int search()
 			k++;
 			j++;
 		}
+		(mass + i)->name[j] = '\0';
 		j = 0;
 
 		while ((RecordLibrary + i)->gerne[j] != '\0')
@@ -549,82 +470,28 @@ int search()
 			k++;
 			j++;
 		}
+		(mass + i)->gerne[j] = '\0';
 		j = 0;
 
-		printf("%-20s|", (mass + i)->author);
-		printf("%-30s|", (mass + i)->name);
-		printf("%s \n", (mass + i)->gerne);
-
-		while (array[cnt] != '\n')
+		flag = 0;
+		if (strstr((mass + i)->author, input1) != NULL ||strstr((mass + i)->author, input2) != NULL || strstr((mass + i)->author, input3) != NULL)
+			flag++;
+		if (strstr((mass + i)->name, input1) != NULL || strstr((mass + i)->name, input2) != NULL || strstr((mass + i)->name, input3) != NULL)
+			flag++;
+		if (strstr((mass + i)->gerne, input1) != NULL || strstr((mass + i)->gerne, input2) != NULL || strstr((mass + i)->gerne, input3) != NULL)
+			flag++;
+		if (flag > 0)
 		{
-			if (array[cnt] >= 'A' && array[cnt] <= 'Z')
-			{
-				array[cnt] += 'a' - 'A';
-			}
-			cnt++;
-		}
-		cnt = 0;
-
-		while (array[cnt] != ' ' || array[cnt] != '\n')
-		{
-			input[k][j] = array[cnt];
-			cnt++;
-			j++;
-		}
-		j = 0;
-		if (array[cnt] == '\n')
-			goto next;
-		cnt++;
-
-		k++;
-
-		while (array[cnt] != ' ' || array[cnt] != '\n')
-		{
-			input[k][j] = array[cnt];
-			cnt++;
-			j++;
-		}
-		j = 0;
-		
-		if (array[cnt] == '\n')
-			goto next;
-		cnt++;
-		k++;
-
-		while (array[cnt] != ' ' || array[cnt] != '\n')
-		{
-			input[k][j] = array[cnt];
-			cnt++;
-			j++;
-		}
-		k++;
-
-	next:;
-
-		while (k != 0)
-		{
-			flag = 0;
-			if (strstr((mass + i)->author, input[k]))
-			{
-				flag++;
-			}
-			if (strstr((mass + i)->name, input[k]))
-			{
-				flag++;
-			}
-			if (strstr((mass + i)->gerne, input[k]))
-			{
-				flag++;
-			}
-			k--;
-			if (flag > 0)
-			{
-				printf("%3d|", i + 1);
-				printf("%-20s|", (RecordLibrary + i)->author);
-				printf("%-30s|", (RecordLibrary + i)->name);
-				printf("%s \n", (RecordLibrary + i)->gerne);
-			}
+			printf("%3d|", i + 1);
+			printf("%-20s|", (RecordLibrary + i)->author);
+			printf("%-30s|", (RecordLibrary + i)->name);
+			printf("%s \n", (RecordLibrary + i)->gerne);
 		}
 	}
+	free(mass);
+	free(input1);
+	free(input2);
+	free(input3);
+	system("pause");
 	return 0;
 }
